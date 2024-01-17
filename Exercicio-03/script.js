ee838ffa43b5b26779df2783041386b5714745b3
RafaelH <rafael0henkel1@gmail.com>
2024-01-16 21:39:54 -0300

    exercicios 3 e 4 feito

--- Exercicio-03/script.js
+++ Exercicio-03/script.js
@@ -6,12 +6,24 @@
 // aluno. Por fim mostre a média desta turma de 2 alunos.
 
 let aluno1 = {
-  Rafael: [8, 7.5],
+  Nome: "Rafael",
+  Nota1: 8,
+  Nota2: 7.5,
 };
 
 let aluno2 = {
-  Adriano: [6, 8],
+  Nome: "Adriano",
+  Nota1: 7,
+  Nota2: 5,
 };
 
 console.log(aluno1, aluno2);
- 
+
+let mediaAluno1 = (aluno1.Nota1 + aluno1.Nota2) / 2;
+console.log(mediaAluno1);
+
+let mediaAluno2 = (aluno2.Nota1 + aluno2.Nota2) / 2;
+console.log(mediaAluno2);
+
+let mediaTurma = (mediaAluno1 + mediaAluno2) / 2;
+console.log(mediaTurma)
--- Exercicio-04/script.js
+++ Exercicio-04/script.js
@@ -1,11 +1,10 @@
 // Crie um cadastro de pessoas onde o usuário informe o nome, idade
 // e se está trabalhando ou não, se a pessoa estiver trabalhando
-
 // pergunte para ele o salário que está ganhando. Para cada pessoa
 // cadastrada, pergunte ao usuário se ele deseja continuar
 // cadastrando ou não. No final, mostre as pessoas que estão
 // desempregadas, as pessoas que estão empregadas separadas
-// pelas que ganham mais que 2500 e menos que 2500.
+// pelas que ganham mais que 2500 e menos que 2500. 
 
 // Exemplo de resultado:
 // Pessoas desempregadas:
@@ -20,7 +19,30 @@
 // Nome: Alessandro, Idade: 28, Salário: 2700
 // Nome: Alessandro, Idade: 28, Salário: 3000
 
-// Este exercício será corrigido na próxima aula com o Tech Helper e deverá
-// ser postado na Class até o horário estipulado da tarefa na plataforma.
-// Crie um arquivo compactado contendo os arquivos com a resolução da
-// atividade e realize o upload no post da atividade no Class.
+let listaDePessoasDesempregadas = [];
+let listaDePessoasEmpregadasMenor = [];
+let listaDePessoasEmpregadasMaior = [];
+let pessoas = {};
+do {
+  let pessoasNovas = { ...pessoas };
+  pessoasNovas.Nome = prompt("Digite seu nome");
+  pessoasNovas.Idade = Number(prompt("Digite sua idade"));
+  let confirmarTrabalho = confirm("Você esta trabalhando?");
+  if (confirmarTrabalho) {
+    pessoasNovas.Salario = Number(prompt("Digite seu salario"));
+    if (pessoasNovas.Salario < 2500) {
+      listaDePessoasEmpregadasMenor.push(pessoasNovas);
+    } else {
+      listaDePessoasEmpregadasMaior.push(pessoasNovas);
+    }
+  } else {
+    listaDePessoasDesempregadas.push(pessoasNovas);
+  }
+} while (confirm("Deseja adicionar outra pessoa?") == true);
+console.log("Pessoas desempregadas:");
+console.log(listaDePessoasDesempregadas);
+console.log("Pessoas empregadas com salários menores que 2500:");
+console.log(listaDePessoasEmpregadasMenor);
+console.log("Pessoas empregadas com salários maiores que 2500:");
+console.log(listaDePessoasEmpregadasMaior);
+
